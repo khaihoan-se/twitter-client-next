d8fb877b3b8747390909fa12c10f341520b99bdc
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-07-23 14:39:34 +0700

    🍓 Editer & ToolEditer 🍓

--- src/components/shared/Editer.tsx
+++ src/components/shared/Editer.tsx
@@ -4,11 +4,21 @@ import { Editor, EditorState, convertToRaw, ContentState } from 'draft-js'
 import { changeText } from '@/utils';
 import ReactHtmlParser from 'react-html-parser'
 import TheEarthIcon from '../icons/TheEarthIcon';
-const TweetPost = () => {
+import ToolEditer from './ToolEditer';
+import classNames from 'classnames';
+// import Image from './Image';
+
+const Editer = () => {
     const [editorState, setEditorState] = useState(() =>
         EditorState.createEmpty()
     );
     const [newPost, setNewPost] = useState<any>([])
+
+    const [check, setCheck] = useState<boolean>(false)
+
+    const [preview, setPreview] = useState<string[]>([])
+    const fileobj: any = [];
+
     const editor = useRef<HTMLDivElement>(null);
     // console.log(convertToRaw(editorState.getCurrentContent()).blocks);
     const handleSubText = () => {
@@ -16,16 +26,52 @@ const TweetPost = () => {
             editor.current.focus();
         }
     }
+    /* Handle Submit data */
     const handleSubmitData = () => {
         setNewPost(convertToRaw(editorState.getCurrentContent()).blocks)
         const editor = EditorState.push(editorState, ContentState.createFromText(''), 'remove-range'); // Reset Input Editor
         setEditorState(editor)
     }
-    
+    /* check the number of characters */
+    const checkLenthInput = () => {
+        const post = convertToRaw(editorState.getCurrentContent()).blocks
+        if(post[0].text == ''){
+            return true
+        }        
+    }
+    /* Change Photo */
+    const onImageChange = (event: any) => {
+        let files = event.target.files;
+        fileobj.push(files);
+        let reader;
+        
+        for (var i = 0; i < fileobj[0].length; i++) {
+            reader = new FileReader();
+            reader.readAsDataURL(fileobj[0][i]);
+            reader.onload = (event: any) => {
+                preview.push(event.target.result);   // update the array instead of replacing the entire value of preview
+
+                setPreview([...preview]); // spread into a new array to trigger rerender
+            } 
+        }
+    }
+    /* check the number of photos */
+    (() => {
+        if(preview.length > 4){
+            setPreview([])
+            setCheck(true)
+            return true
+        } 
+    })()
+    /* pile of notifications after 3 seconds */
+    setTimeout(() => {
+        setCheck(false)
+    }, 3000)
+
     return (
         <React.Fragment>
             <div className='bg-th-background py-[4px] max-h-[100vh] h-auto'>
-                <div className='px-[14px] w-full h-full'>
+                <div className='px-[14px] w-full h-full border-b-[1px] border-tt-border-color'>
                     <div className='flex-row flex'>
                         <div className='pt-[4px] basis-[43px] mr-[11px] grow-0'>
                             <Avatar src='/unnamed.gif' className='w-[43px] h-[43px]' />
@@ -38,10 +84,27 @@ const TweetPost = () => {
                                     placeholder="What’s happening?" 
                                 />
                             </div>
+                            {preview.length > 0 ? (
+                                <div className='w-full h-auto bg-slate-500'>
+                                    {/* <div className='w-full h-auto flex items-center'>
+                                        <div className='w-[50%] h-full'><img src={photo[0].name} alt='' className='w-full h-full' /></div>
+                                        <div className='w-[50%] h-full'><img src={photo[1].name} alt='' className='w-full h-full' /></div>
+                                    </div>
+                                    <div className='w-full h-auto flex items-center'>
+                                        <div className='w-[50%] h-full'><img src={photo[2].name} alt='' className='w-full h-full' /></div>
+                                        <div className='w-[50%] h-full'><img src={photo[3].name} alt='' className='w-full h-full' /></div>
+                                    </div> */}
+                                    {preview.map((item: any, index: number) => (
+                                        <img src={item} key={index} alt='' className='w-full h-full' />
+                                    ))}
+                                </div>
+                                ):
+                                null
+                            }
                             {/* Everyone can reply */}
                             <div className='w-full ml-[-8px] border-b-[1px] border-tt-border-color'>
                                 <div className='pb-[12px] min-h-[24px] text-tt-main-color font-bold text-[15px] items-center'>
-                                    <div className='hover:bg-tt-hover-maincl-color inline-flex items-center py-[2px] px-[12px] rounded-full cursor-pointer'  onClick={handleSubmitData}>
+                                    <div className='hover:bg-tt-hover-maincl-color inline-flex items-center py-[2px] px-[12px] rounded-full cursor-pointer'>
                                         <TheEarthIcon className='w-[16px] h-[16px] mr-[4px]' />
                                         <span>Everyone can reply</span>
                                     </div>
@@ -49,9 +112,19 @@ const TweetPost = () => {
                             </div>
                             {/* Tab Tools */}
                             <div>
-                                Tab Tools
+                                <div className='flex-wrap z-[1] flex items-center justify-between flex-row bg-tt-bg-color mb-[16px]'>
+                                    <ToolEditer onImageChange={onImageChange} />
+                                    <div className={classNames(
+                                        'mt-[12px] bg-tt-main-color ml-[12px] min-w-[36px] min-h-[36px] outline-none px-[16px] rounded-full flex items-center justify-center cursor-pointer',
+                                        checkLenthInput() && 'opacity-50 pointer-events-none'
+                                    )} onClick={handleSubmitData}>
+                                        <div className='text-[#fff] font-bold text-[15px]'>
+                                            <span>Tweet</span>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-
+                            {check && <div>chỉ 4 ảnh</div>}
                             <div>
                                 {newPost.map((item: any, index: number) => (
                                     <p className='text-[15px]' key={index}>
@@ -67,4 +140,4 @@ const TweetPost = () => {
     )
 }
 
-export default React.memo(TweetPost)
+export default React.memo(Editer)
---
+++ src/components/shared/ToolEditer.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import MediaIcon from '../icons/MediaIcon';
+import GifIcon from '../icons/GifIcon';
+import PollIcon from '../icons/PollIcon';
+import EmojiIcon from '../icons/EmojiIcon';
+import ScheduleIcon from '../icons/ScheduleIcon';
+import LocationIcon from '../icons/LocationIcon';
+
+interface ToolEditerProps {
+    onImageChange: any;
+}
+const ToolEditer = ({ onImageChange }: ToolEditerProps) => {
+    return (
+        <div className='flex items-center mt-[12px]'>
+            {/* MediaIcon */}
+            <div className='toolediter--media min-w-[36px] min-h-[36px] rounded-full hover:bg-tt-hover-maincl-color flex items-center justify-center cursor-pointer relative'>
+                <MediaIcon className='w-[20px] h-[20px] text-tt-main-color' />
+                <span className='hidden px-[4px] py-[2px] bg-slate-500 absolute top-[100%] text-[12px] rounded-sm text-white'>Media</span>
+                <input accept='image/jpeg,image/png,image/webp,image/gif' multiple type='file' className='absolute w-full h-full opacity-0 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] cursor-pointer z-40' onChange={onImageChange} />
+            </div>
+            {/* GifIcon */}
+            <div className='toolediter--gif min-w-[36px] min-h-[36px] rounded-full hover:bg-tt-hover-maincl-color flex items-center justify-center cursor-pointer relative'>
+                <GifIcon className='w-[20px] h-[20px] text-tt-main-color' />
+                <span className='hidden px-[4px] py-[2px] bg-slate-500 absolute top-[100%] text-[12px] rounded-sm text-white'>GIF</span>
+            </div>
+            {/* PollIcon */}
+            <div className='toolediter--poll min-w-[36px] min-h-[36px] rounded-full hover:bg-tt-hover-maincl-color flex items-center justify-center cursor-pointer relative'>
+                <PollIcon className='w-[20px] h-[20px] text-tt-main-color' />
+                <span className='hidden px-[4px] py-[2px] bg-slate-500 absolute top-[100%] text-[12px] rounded-sm text-white'>Poll</span>
+            </div>
+            {/* EmojiIcon */}
+            <div className='toolediter--emoji min-w-[36px] min-h-[36px] rounded-full hover:bg-tt-hover-maincl-color flex items-center justify-center cursor-pointer relative'>
+                <EmojiIcon className='w-[20px] h-[20px] text-tt-main-color' />
+                <span className='hidden px-[4px] py-[2px] bg-slate-500 absolute top-[100%] text-[12px] rounded-sm text-white'>Emoji</span>
+            </div>
+            {/* ScheduleIcon */}
+            <div className='toolediter--schedule min-w-[36px] min-h-[36px] rounded-full hover:bg-tt-hover-maincl-color flex items-center justify-center cursor-pointer relative'>
+                <ScheduleIcon className='w-[20px] h-[20px] text-tt-main-color' />
+                <span className='hidden px-[4px] py-[2px] bg-slate-500 absolute top-[100%] text-[12px] rounded-sm text-white'>Schedule</span>
+            </div>
+            {/* LocationIcon */}
+            <div className='toolediter--location min-w-[36px] min-h-[36px] rounded-full hover:bg-tt-hover-maincl-color flex items-center justify-center cursor-pointer relative'>
+                <LocationIcon className='w-[20px] h-[20px] text-tt-main-color' />
+                <span className='hidden px-[4px] py-[2px] bg-slate-500 absolute top-[100%] text-[12px] rounded-sm text-white'>Location</span>
+            </div>
+        </div>
+    )
+}
+
+export default React.memo(ToolEditer)
