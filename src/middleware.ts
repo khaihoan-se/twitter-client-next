6f78a2066591dc478a8ea2c55ec637e35957edbb
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-08-08 23:03:21 +0700

    🍇🍒 middleware auth router 🍇🍒

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+// middleware.ts
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+   // Setting url on the request
+   const url = request.nextUrl.clone()
+   // Getting cookies from the request
+   const cookieToken = request.cookies.get('token')
+   // check if the token is valid page Login
+   if(url.pathname === '/login') {
+      if(cookieToken) {
+         return NextResponse.redirect(new URL('/', request.url))
+      }
+   }
+   // check if the token is valid page Home
+   if(url.pathname === '/') {
+      if(!cookieToken) {
+         return NextResponse.redirect(new URL('/login', request.url))
+      }
+   }
+
+   return NextResponse.next()
+}
