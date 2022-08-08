d7465931516d0cdab0ab76dc53cf6c62ea83eb08
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-08-08 23:01:00 +0700

    🍭🍦 domains next.config.js 🍭🍦

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     images: {
-      domains: ['scontent.fsgn2-6.fna.fbcdn.net', 'scontent.fsgn13-4.fna.fbcdn.net'],
+      domains: [
+        'scontent.fsgn2-6.fna.fbcdn.net', 
+        'scontent.fsgn13-4.fna.fbcdn.net', 
+        'localhost',
+        'scontent.fsgn2-4.fna.fbcdn.net'
+      ],
     },
 }
