1692d188ddfb76bf731281cc7ebc684b9b0e1ca4
khaihoan-se <khaihoan.softwareengineer@gmail.com>
2022-09-09 23:41:51 +0700

    add

--- src/components/features/home/PostList.tsx
+++ src/components/features/home/PostList.tsx
@@ -10,33 +10,35 @@ interface PostListProps {
 const PostList = ({ data }: PostListProps) => {
    const [ posts, setPosts ] = useState(data)
    const [hasMore, sethasMore] = useState(true);
-   const [ page, setpage ] = useState(2);
-
-   const fetchComments = async () => {
-      const res = await axios.get(`http://localhost:5000/api/get-posts?page=${page}&limit=5`)
-      const data = res.data.posts
-      return data
-   }
+   const [ page, setPage ] = useState(2);
    
-   const fetchData = async () => {
-      console.log('addd');
-      
-      const commentsFormServer = await fetchComments();
-      setPosts([...posts, ...commentsFormServer]);      
-      if (commentsFormServer.length === 0 || commentsFormServer.length < 10) {
-         sethasMore(false);
-      }
-      setpage(page + 1);
-      console.log(page);
-      
-   }
+   // const fetchComments = async () => {
+   //    const res = await axios.get(`http://localhost:5000/api/get-posts?page=${page}&limit=5`)
+   //    const data = res.data.posts
+   //    return data
+   // }
+   // const fetchData = async () => {
+   //    // const commentsFormServer = await fetchComments();
+   //    const res = await axios.get(`http://localhost:5000/api/get-posts?page=${page}&limit=5`)
+   //    const commentsFormServer = res.data.posts
+   //    setPosts((post: any) => [...post, ...commentsFormServer]);   
+   //    // if (commentsFormServer.length === 0 || commentsFormServer.length < 5) {
+   //    //    sethasMore(false);
+   //    // }
+   //    setPage(page + 1)
+   // }
+   
    return (
       <InfiniteScroll
          dataLength={posts.length}
          next={fetchData}
          hasMore={hasMore}
+         inverse={true}
          loader={<div>Loading...</div>}
          endMessage={<div>Hết bài ....</div>}
+         style={{
+            overflow: 'initial',
+         }}
       >
          {posts.map((item: any, index: number) => (
             <CardPost description={item.description} images={item.images} key={index} />
